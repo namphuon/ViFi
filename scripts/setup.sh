a789a940d10cfecdcf5af762076aa2548e89d464
Nam-phuong Nguyen <namphuon@notifier@genomemaster.ucsd.edu>
2019-03-22 07:30:26 -0700

    adding setup file

---
+++ scripts/setup.sh
@@ -0,0 +1,34 @@
+#Get ViFi
+git clone https://github.com/namphuon/ViFi.git
+cd ViFi
+VIFI_DIR=`pwd`
+
+#Get data repos
+wget https://raw.githubusercontent.com/circulosmeos/gdown.pl/master/gdown.pl
+perl gdown.pl "https://drive.google.com/open?id=0ByYcg0axX7udUDRxcTdZZkg0X1k" data_repo.tar.gz
+tar zxf data_repo.tar.gz
+perl gdown.pl "https://drive.google.com/open?id=0Bzp6XgpBhhghSTNMd3RWS2VsVXM" data.zip
+unzip data.zip
+
+#Set up environmental variables
+echo export VIFI_DIR=$VIFI_DIR >> ~/.bashrc
+echo export AA_DATA_REPO=$PWD/data_repo >> ~/.bashrc
+echo export REFERENCE_REPO=$PWD/data >> ~/.bashrc
+source ~/.bashrc
+
+#Set up reference for alignment
+cat $AA_DATA_REPO//hg19/hg19full.fa $REFERENCE_REPO/hpv/hpv.unaligned.fas > $REFERENCE_REPO/hpv/hg19_hpv.fas
+docker run -v $REFERENCE_REPO/hpv/:/home/hpv/ docker.io/namphuon/vifi bwa index /home/hpv/hg19_hpv.fa
+#bwa index $REFERENCE_REPO/hpv/hg19_hpv.fas
+ls $VIFI_DIR/data/hpv/hmms/hmmbuild.[0-9].hmm > $VIFI_DIR/data/hpv/hmms/hmms.txt
+source ~/.bashrc
+
+#Pull the Docker file
+docker pull docker.io/namphuon/vifi 
+
+#Run ViFi under docker mode
+python $VIFI_DIR/scripts/run_vifi.py --cpus 2 --hmm_list $VIFI_DIR/data/hpv/hmms/hmms.txt -f $VIFI_DIR/test/data/test_R1.fq.gz -r $VIFI_DIR/test/data/test_R2.fq.gz -o $VIFI_DIR/tmp/docker/ --docker
+
+
+
+
